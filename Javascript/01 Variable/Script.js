08c93851f4b41e10ad6aae68419bb2b863013434
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-09-15 21:53:12 +0530

    Variable

---
+++ Javascript/01 Variable/Script.js
@@ -0,0 +1,32 @@
+console.log("Hey This is JS Tutorial")
+
+var a = 5;
+var b  = 6;
+var c = "Harry";
+
+console.log(a + b)
+
+console.log(typeof a, typeof b, typeof c)
+
+let x = "harry Potrer";
+let y = 12;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q,typeof r)
+
+let O = {
+   "name" : "Harry",
+   "job code" : 5600,
+   "is_handsome" : true
+}
+console.log(O);
+
+O.Salary = "100crores";
+console.log(O);
+
+O.Salary = "500crores";
+console.log(O);
