d5eab40e117e6cee581c50388de1bbe8a848e5ae
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-09-17 23:26:16 +0530

    Loop

---
+++ 03 Loops/Script.js
@@ -0,0 +1,38 @@
+console.log("This is Loops Tutorial")
+
+// let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for(let i = 0; i < 30; i++)
+// {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name : "Harry",
+//     role : "Programmer",
+//     company : "Google"
+// }
+
+// for(const key in obj){
+//     console.log(key)
+// }
+
+// for(const c of "Harry"){
+//     console.log(c)
+// }
+
+// let i = 0;
+// while(i < 20){
+//     console.log(i)
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
