1935cc5fd4b1a2a839fdf8c85237aa07a25cb36d
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-09-18 21:20:17 +0530

    Function

---
+++ Javascript/04 Function/Script.js
@@ -0,0 +1,52 @@
+// function nice(name){
+//     console.log("Hey " + name + " you are nice!")
+//     console.log("Hey " + name + " you are good!")
+//     console.log("Hey " + name + " your tshirt is nice!")
+//     console.log("Hey " + name + " your course is good too!")
+// }
+
+// nice("Rohan")
+// nice("Shivam")
+
+/* ========================================================================*/
+
+// function sum(a,b){
+//     /* console.log(a + b); */
+//     return a + b;
+// }
+
+// result1 = sum(3, 5)
+// result2= sum(7, 5)
+// result3= sum(3, 13)
+
+// console.log("The Sum of This Number is: ", result1)
+// console.log("The Sum of This Number is: ", result2)
+// console.log("The Sum of This Number is: ", result3)
+
+/* ========================================================================*/
+
+// function sum(a,b,c = 3){                                 //Default Value c = 3
+//     /* console.log(a + b); */
+//     return a + b + c;
+// }
+
+// result1 = sum(3, 5)
+// result2= sum(7, 5)
+// result3= sum(3, 13)
+
+// console.log("The Sum of This Number is: ", result1)
+// console.log("The Sum of This Number is: ", result2)
+// console.log("The Sum of This Number is: ", result3)
+
+/* ========================================================================*/
+
+/*     Arrow Function           */
+
+const func1 = (x)=>{
+    console.log("I am an arrow function",x)
+}
+
+func1(34);
+func1(66);
+func1(84);
+
