1063946f7f98e1904400e3fc0c5ac5449018db20
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-09-19 21:19:27 +0530

    String

---
+++ Javascript/05 String/Script.js
@@ -0,0 +1,35 @@
+console.log("This is String Tutorial")
+
+ let a = "Harry";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+/*  ===============================================================================   */ 
+
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+
+ /*Template Literal*/
+ console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+/*  ===============================================================================   */ 
+
+ let b = "ShivamSh"
+ console.log(b.toUpperCase())
+ console.log(b.toLowerCase())
+ console.log(b.length)
+ console.log(b.slice(1,5))
+ console.log(b.slice(1))
+ console.log(b.charAt(1))
+ console.log(b.indexOf("h"))
+ console.log(b.startsWith("S"))
+ console.log(b.endsWith("h"))
+
+ console.log(b.replace("Sh", "77"))
+ console.log(b.concat(a, "Aishwarya", "Rahul", "Priya"))
