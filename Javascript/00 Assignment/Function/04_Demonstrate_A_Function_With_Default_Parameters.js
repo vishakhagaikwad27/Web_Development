4e693ec48e536362a82cb4d991145ef2fc3663bd
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-10-25 21:54:33 +0530

    Function

---
+++ Javascript/00 Assignment/Function/04_Demonstrate_A_Function_With_Default_Parameters.js
@@ -0,0 +1,7 @@
+function defaultMessage(name = "Guest",Message = "Welcome To Our Javascript"){
+    console.log(`Hellow ${name}, ${Message}`);
+};
+
+defaultMessage();
+defaultMessage("Vishakha");
+defaultMessage("Vishakha","Have A Nice Day");
