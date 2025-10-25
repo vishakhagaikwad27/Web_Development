35a4c81aa30610739bcf6426f73695e5cf8c75d7
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-10-25 21:55:55 +0530

    While Loop

---
+++ Javascript/00 Assignment/While Loop/01_Print_Numbers_Form_1To_10_Using_A_While_Loop.js
@@ -0,0 +1,13 @@
+// Print Numbers From 1 To 10 Using A While Loop
+
+// let i = 0;
+// while( i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+let i = 1;
+while( i <= 10){
+    console.log(i);
+    i++;
+}
