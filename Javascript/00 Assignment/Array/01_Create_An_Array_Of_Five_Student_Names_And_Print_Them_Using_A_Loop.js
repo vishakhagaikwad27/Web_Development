3503e18d96e8ced0c49eb293b1045bb1deb7c25c
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-10-25 21:53:32 +0530

    Array

---
+++ Javascript/00 Assignment/Array/01_Create_An_Array_Of_Five_Student_Names_And_Print_Them_Using_A_Loop.js
@@ -0,0 +1,6 @@
+var Student = ["Vishakha","Sakshi","Trisha","Shivani","Sneha"];
+
+
+for(let i = 0; i<Student.length;i++){
+    console.log(Student[i]);
+}
---
+++ Javascript/00 Assignment/Array/03_Remove_duplicate_values_from_an_array.js
@@ -0,0 +1,14 @@
+// Remove duplicate values from an array (hint: use Set).
+
+let Numbers = [1,2,3,4,2,3,1];
+
+ let mySet = new Set(Numbers);
+
+console.log(mySet);
+
+ let numbers = [1, 2, 3, 2, 4, 1, 5];
+
+
+let uniqueNumbers = [...new Set(numbers)];
+
+console.log(uniqueNumbers);
