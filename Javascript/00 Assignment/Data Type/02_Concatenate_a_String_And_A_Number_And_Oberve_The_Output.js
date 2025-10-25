2f95ad12a9507c084be078b9f168b5786ffe3a8a
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-10-25 21:52:54 +0530

    Dattatype

---
+++ Javascript/00 Assignment/Data Type/02_Concatenate_a_String_And_A_Number_And_Oberve_The_Output.js
@@ -0,0 +1,13 @@
+// let  name = "Vishakha";
+// let num = 1500;
+
+// let result = name + num ;
+// console.log(result);
+
+
+let name = "Vishakha";
+let age = 22;
+
+let result = name + age;
+
+console.log(result);
