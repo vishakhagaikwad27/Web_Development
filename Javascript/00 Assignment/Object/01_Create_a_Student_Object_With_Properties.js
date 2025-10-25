97508d435fb0bc885d4f8a968cdf3dfd24199e75
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-10-25 21:55:00 +0530

    Object

---
+++ Javascript/00 Assignment/Object/01_Create_a_Student_Object_With_Properties.js
@@ -0,0 +1,9 @@
+// Create a student object with properties: name, age, and marks.
+
+let selfInfo = {
+    name : "Vishakha",
+    age :21,
+    marks : 95,
+    Course : "BCS"
+};
+console.log(selfInfo);
---
+++ Javascript/00 Assignment/Object/03_Create a nested object (student with address) and print the city property.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Vishakha",
+    age: 20,
+    marks: 90,
+    course: "Bcs",
+    address: {
+        city: "Karad",
+        state: "Maharashtra",
+        pin: 415110
+    }
+};
+
+// Access nested property
+console.log("City: " + student.address.city);
