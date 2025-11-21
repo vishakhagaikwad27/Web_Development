47064e85dbcb4ca5992f71f9b3b3da28d2bc4531
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-11-21 11:25:50 +0530

    Check if a key exists using .has()

---
+++ Javascript/00 Assignment/Map/03_Check if a key exists using .has().js
@@ -0,0 +1,9 @@
+///Check if a key exists using .has().
+ 
+let Array =  new Map();
+Array.set("name","Priya"),
+Array.set("age",22);
+Array.set("Course","BCS");
+
+console.log(Array.has("age"));
+console.log(Array.has("PRN No"));
