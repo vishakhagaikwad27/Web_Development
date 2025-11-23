041cb9bbfccd4ffc25daf7b7fd359f4d3c95b30b
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-11-23 21:25:12 +0530

    Get a value using get()

---
+++ Javascript/00 Assignment/Map/04_Get a value using get().js
@@ -0,0 +1,10 @@
+///Get a value using .get().
+ let Array = new Map();
+
+ Array.set("name","Anamika");
+ Array.set("Age",22);
+ Array.set("Course","BCS");
+
+ console.log(Array.get("name"));
+ console.log(Array.get("Age"));
+ console.log(Array.get("Course"));
