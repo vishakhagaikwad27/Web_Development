83b5107c2bbe540e8617d672073ca63c66d7b565
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2025-11-28 23:53:37 +0530

    Add a new value, delete one value

---
+++ Javascript/00 Assignment/Set/01_Add a new value, delete one value, and print final Set.js
@@ -0,0 +1,8 @@
+//Add a new value, delete one value, and print final Set.
+
+let Number = new Set ([10,20,30,40,50,50,80,89]);
+Number.add(646);
+Number.add(1459);
+//console.log(Number);
+Number.delete(89);
+console.log(Number);
