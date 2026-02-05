f59f26ed73f71f5e888c902140771d201808ed12
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2026-02-05 22:53:17 +0530

    Arithmatic Operator

---
+++ Javascript/23 Javascript Practice/02 Operators/01 Arithmatic Operators.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 2;
+
+let Result = a + b;
+
+console.log(Result);
+
+Result = a - b;
+console.log(Result);
+
+Result = a * b;
+console.log(Result);
+
+Result = a / b;
+console.log(Result);
+
+Result = a % b;
+console.log(Result);
+
+Result = a ** b;
+console.log(Result);
+
+//10*10*10
+//10^3
