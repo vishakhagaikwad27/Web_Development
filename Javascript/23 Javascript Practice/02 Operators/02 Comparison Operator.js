321472a78bd45667c3f60a931e4f4ae93d5289ee
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2026-02-06 22:13:40 +0530

    Comparison Operator

---
+++ Javascript/23 Javascript Practice/02 Operators/02 Comparison Operator.js
@@ -0,0 +1,18 @@
+let Num1 = true;
+let Num2 = '1';
+
+if(Num1 == Num2){
+    console.log("True")
+}
+
+else{
+    console.log("False")
+}
+
+if(Num1 === Num2){
+    console.log("True")
+}
+
+else{
+    console.log("False")
+}
