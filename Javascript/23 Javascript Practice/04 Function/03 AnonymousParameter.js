5358ea1d62a32b88e86acc317d9a7b00f3370956
Vishakha Gaikwad <vishakhagaikwad874@gmail.com>
2026-02-11 22:38:14 +0530

    AnonymousParameter

---
+++ Javascript/23 Javascript Practice/04 Function/03 AnonymousParameter.js
@@ -0,0 +1,14 @@
+function Calc(no1,no2){
+    console.log(no1 + no2);
+}
+
+let num1 = 200;
+let num2 = 50;
+
+Calc(num1,num2);
+
+let add = function(num1,num2){
+    return num1 + num2;
+}
+
+console.log(add(10,20));
